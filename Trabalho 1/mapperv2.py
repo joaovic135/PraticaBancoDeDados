130a4855aae671957cb0081b606c0bc072f2479a
joaovic135 <jvba@icomp.ufam.edu.br>
2023-08-15 14:06:06 -0400

    removepontuacao

--- Trabalho 1/mapperv2.py
+++ Trabalho 1/mapperv2.py
@@ -4,26 +4,26 @@ import sys
 import re
 
 class Mapper:
-    def __init__(self):
-        self.H = {}
+  def __init__(self):
+      self.H = {}
 
-    def map(self, line):
-        docid, doc = line.strip().split('\t')
-        terms = doc.split()
-        for term in terms:
-            cleaned_term = re.sub(r'[.,?!()"\'$#]', '', term.lower())
-            if re.match(r'^[a-zA-Z]+$', cleaned_term):  # Verifica se a palavra tem apenas letras
-                if cleaned_term in self.H:
-                    self.H[cleaned_term] += 1
-                else:
-                    self.H[cleaned_term] = 1
+  def map(self, line):
+    docid, doc = line.strip().split('\t')
+    terms = doc.split()
+    for term in terms:
+      cleaned_term = re.sub(r'[.,?!()"\'$#]', '', term.lower())
+      if re.match(r'^[a-zA-Z]+$', cleaned_term):  # Verifica se a palavra tem apenas letras
+        if cleaned_term in self.H:
+          self.H[cleaned_term] += 1
+        else:
+          self.H[cleaned_term] = 1
 
-    def close(self):
-        for term, count in self.H.items():
-            print(f"{term}\t{count}")
+  def close(self):
+    for term, count in self.H.items():
+      print(f"{term}\t{count}")
 
 if __name__ == "__main__":
-    mapper = Mapper()
-    for line in sys.stdin:
-        mapper.map(line)
-    mapper.close()
+  mapper = Mapper()
+  for line in sys.stdin:
+      mapper.map(line)
+  mapper.close()

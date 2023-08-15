a5d11d6b982f90a00689dcb2fa008afe90280838
joaovic135 <jvba@icomp.ufam.edu.br>
2023-08-15 12:58:03 -0400

    removepontuacao

---
+++ Trabalho 1/Contador.py
@@ -0,0 +1,25 @@
+import sys
+
+def ContadorDePalavras(documento):
+    palavra_numero = {}
+    
+    with open(documento, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) >= 2:
+                palavra = " ".join(parts[:-1])
+                numero = int(parts[-1])
+                
+                if palavra in palavra_numero:
+                    palavra_numero[palavra] += numero
+                else:
+                    palavra_numero[palavra] = numero
+    
+    palavras_ordenadas = sorted(palavra_numero, key=palavra_numero.get, reverse=True)
+    
+    for palavra in palavras_ordenadas[:10]:
+        print(f'{palavra}: {palavra_numero[palavra]}')
+
+
+arquivo_path = 'part-00000'  
+ContadorDePalavras(arquivo_path)
